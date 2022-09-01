743a3aa0f76b21c5ffa887cf1c93ef546facb409
PulakBala <pulakbala0524@gmail.com>
2022-09-01 11:31:58 +0600

    try await

--- js/countries.js
+++ js/countries.js
@@ -5,16 +5,7 @@ const loadCountries = () => {
 }
 
 const displayCountries = (user) => {
-    // const cuntriesContainer = document.getElementById('countries-container');
-    // for(const use of user) {
-    //     console.log(use)
-    //     const newDiv = document.createElement('div');
-    //     newDiv.innerHTML = `
-    //         <h4>Country Name: ${use.name.common}  </h4>
-    //         <span> Official Name: ${use.name.official} </span>
-    //     `;
-    //     cuntriesContainer.appendChild(newDiv)
-    // }
+ 
     const cuntriesContainer = document.getElementById('countries-container');
     user.forEach(country => {
         const countryDiv = document.createElement('div');
